66d6be66a4b2387a66b34fe0e3e396b057a6a2ea
Berksayit <berk-sayit@hotmail.com>
2024-08-01 23:15:40 +0300

    update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 load_dotenv()
 
-# OMDb API key
+# OMDb API anahtarı
 OMDB_API_KEY = os.getenv('OMDB_API_KEY')
 OMDB_API_URL = 'http://www.omdbapi.com/'
 
@@ -15,22 +15,20 @@ OMDB_API_URL = 'http://www.omdbapi.com/'
 def index():
     return render_template('index.html')
 
-
-
 @app.route('/api/search', methods=['POST'])
 def search_api():
     data = request.get_json()
     title = data.get('title')
     if not title:
-        return jsonify({'error': 'Movie title is required.'}), 400
+        return jsonify({'error': 'Film başlığı gereklidir.'}), 400
     
-    # Send request to OMDb API
+    # OMDb API'ye istek gönder
     try:
         response = requests.get(OMDB_API_URL, params={'t': title, 'apikey': OMDB_API_KEY})
-        response.raise_for_status()  # Capture HTTP error codes
+        response.raise_for_status()  # HTTP hata kodlarını yakala
         data = response.json()
 
-        # Check the API response
+        # API yanıtını kontrol et
         if data['Response'] == 'True':
             movie_info = {
                 "Title": data.get('Title'),
@@ -45,8 +43,8 @@ def search_api():
         else:
             return jsonify({'error': data['Error']}), 400
     except requests.RequestException as e:
-        # Handle HTTP request errors
-        return jsonify({'error': f"HTTP Error: {e}"}), 500
+        # HTTP istek hatalarını işle
+        return jsonify({'error': f"HTTP Hatası: {e}"}), 500
 
 if __name__ == '__main__':
     app.run(debug=True)
