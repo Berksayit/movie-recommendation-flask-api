26378d6f1b68f34b4d13d6883f0418d9a2483c81
Berksayit <berk-sayit@hotmail.com>
2024-08-01 19:21:10 +0300

    update app.py

--- app.py
+++ app.py
@@ -1,15 +1,11 @@
-from flask import Flask, jsonify, request, render_template
+from flask import Flask, request, render_template
 import requests
-from googletrans import Translator
 
 app = Flask(__name__)
 
-# OMDb API anahtarınızı buraya ekleyin
+# OMDb API key
 OMDB_API_KEY = '77ae9d39'
 OMDB_API_URL = 'http://www.omdbapi.com/'
-
-# Çeviri için Google Translator kullanımı
-translator = Translator()
 
 @app.route('/')
 def index():
@@ -19,40 +15,31 @@ def index():
 def search():
     title = request.args.get('title')
     if not title:
-        return render_template('index.html', error="Film adı girilmelidir.")
+        return render_template('index.html', error="Movie title is required.")
     
-    # OMDb API'ye istek gönder
+    # Send request to OMDb API
     try:
         response = requests.get(OMDB_API_URL, params={'t': title, 'apikey': OMDB_API_KEY})
-        response.raise_for_status()  # HTTP hata kodlarını yakalamak için
+        response.raise_for_status()  # Capture HTTP error codes
         data = response.json()
 
-        # API yanıtını kontrol et
+        # Check the API response
         if data['Response'] == 'True':
-            # Çeviriyi yap
-            plot_translation = translator.translate(data.get('Plot'), src='en', dest='tr').text
-
-            # Türleri Türkçeye çevir
-            genres = data.get('Genre', '').split(', ')
-            genre_translation_text = ', '.join(
-                translator.translate(g, src='en', dest='tr').text for g in genres
-            )
-
             movie_info = {
-                "Film Adı": data.get('Title'),
-                "Yıl": data.get('Year'),
-                "Tür": genre_translation_text,
-                "Yönetmen": data.get('Director'),
-                "Konu": plot_translation,
-                "Oyuncular": data.get('Actors'),
-                "IMDb Reytingi": data.get('imdbRating')
+                "Title": data.get('Title'),
+                "Year": data.get('Year'),
+                "Genre": data.get('Genre'),
+                "Director": data.get('Director'),
+                "Plot": data.get('Plot'),
+                "Actors": data.get('Actors'),
+                "IMDb Rating": data.get('imdbRating')
             }
             return render_template('index.html', movie=movie_info)
         else:
             return render_template('index.html', error=data['Error'])
     except requests.RequestException as e:
-        # API isteği sırasında bir hata oluştuysa
-        return render_template('index.html', error=f"HTTP Hata: {e}")
+        # Handle HTTP request errors
+        return render_template('index.html', error=f"HTTP Error: {e}")
 
 if __name__ == '__main__':
     app.run(debug=True)
