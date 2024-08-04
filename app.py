48f50819eaa65a196fd899152d61211b01685d3e
Berksayit <berk-sayit@hotmail.com>
2024-08-04 15:54:12 +0300

    update app.py

--- app.py
+++ app.py
@@ -1,21 +1,24 @@
 from flask import Flask, request, render_template, jsonify
 import requests
-import os
 from dotenv import load_dotenv
+import os
+
+# Load environment variables from .env file
+load_dotenv()
 
 app = Flask(__name__)
 
-load_dotenv()
-
-# OMDb API key
+# OMDb API key from .env file
 OMDB_API_KEY = os.getenv('OMDB_API_KEY')
 OMDB_API_URL = 'http://www.omdbapi.com/'
+
+# TMDb API key from .env file
+TMDB_API_KEY = os.getenv('TMDB_API_KEY')
+TMDB_API_URL = 'https://api.themoviedb.org/3'
 
 @app.route('/')
 def index():
     return render_template('index.html')
-
-
 
 @app.route('/api/search', methods=['POST'])
 def search_api():
@@ -24,13 +27,11 @@ def search_api():
     if not title:
         return jsonify({'error': 'Movie title is required.'}), 400
     
-    # Send request to OMDb API
     try:
         response = requests.get(OMDB_API_URL, params={'t': title, 'apikey': OMDB_API_KEY})
-        response.raise_for_status()  # Capture HTTP error codes
+        response.raise_for_status()
         data = response.json()
 
-        # Check the API response
         if data['Response'] == 'True':
             movie_info = {
                 "Title": data.get('Title'),
@@ -45,8 +46,28 @@ def search_api():
         else:
             return jsonify({'error': data['Error']}), 400
     except requests.RequestException as e:
-        # Handle HTTP request errors
-        return jsonify({'error': f"HTTP Error: {e}"}),500
+        return jsonify({'error': f"HTTP Error: {e}"}), 500
+
+@app.route('/api/top20', methods=['GET'])
+def top20_movies():
+    try:
+        response = requests.get(f'{TMDB_API_URL}/movie/top_rated', params={'api_key': TMDB_API_KEY, 'language': 'en-US', 'page': 1})
+        response.raise_for_status()
+        data = response.json()
+        
+        top20 = data['results'][:20]
+        
+        top20_movies = [{
+            'Title': movie['title'],
+            'Release Date': movie['release_date'],
+            'Rating': movie['vote_average'],
+            'Overview': movie['overview'],
+            'Poster': f"https://image.tmdb.org/t/p/w500{movie['poster_path']}" if movie.get('poster_path') else 'https://via.placeholder.com/150'
+        } for movie in top20]
+        
+        return jsonify(top20_movies)
+    except requests.RequestException as e:
+        return jsonify({'error': f"HTTP Error: {e}"}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True) 
+    app.run(debug=True)
