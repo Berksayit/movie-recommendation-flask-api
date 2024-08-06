9124701c872681d4a58dd015aaf636fbe28c3c0f
Berksayit <berk-sayit@hotmail.com>
2024-08-06 04:36:52 +0300

    created config.py

---
+++ config.py
@@ -0,0 +1,7 @@
+import os
+
+class Config:
+    OMDB_API_KEY = os.getenv('OMDB_API_KEY')
+    OMDB_API_URL = 'http://www.omdbapi.com/'
+    TMDB_API_KEY = os.getenv('TMDB_API_KEY')
+    TMDB_API_URL = 'https://api.themoviedb.org/3'
