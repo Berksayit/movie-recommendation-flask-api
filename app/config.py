dae2584d596dff45bb6d48db9f39ded356964189
Berksayit <berk-sayit@hotmail.com>
2024-08-09 04:43:37 +0300

    updated config.py

--- app/config.py
+++ app/config.py
@@ -1,7 +1,8 @@
 import os
 
-class Config:
-    OMDB_API_KEY = os.getenv('OMDB_API_KEY')
-    OMDB_API_URL = 'http://www.omdbapi.com/'
-    TMDB_API_KEY = os.getenv('TMDB_API_KEY')
-    TMDB_API_URL = 'https://api.themoviedb.org/3'
+Config = { 
+    "OMDB_API_KEY": os.getenv("OMDB_API_KEY"),
+    "OMDB_API_URL": "http://www.omdbapi.com/",
+    "TMDB_API_KEY": os.getenv("TMDB_API_KEY"),
+    "TMDB_API_URL": "https://api.themoviedb.org/3",
+}
