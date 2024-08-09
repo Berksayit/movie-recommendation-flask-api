453e1b81fb80b3536f24dc73f4dd45556aa5862b
Berksayit <berk-sayit@hotmail.com>
2024-08-09 04:28:24 +0300

    updated __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,18 +1,6 @@
-from flask import Flask
-from .views.movie_views import movie_views
-from .views.top20_views import top20_views
-from dotenv import load_dotenv
-import os
+from flask import Flask 
 
-def create_app():
-    # Load environment variables from .env file
-    load_dotenv()
+app = Flask(__name__)
 
-    app = Flask(__name__)
-    app.config.from_object('config.Config')
-
-    # Register Blueprints
-    app.register_blueprint(movie_views)
-    app.register_blueprint(top20_views)
-
-    return app
+from .config import Config
+from .views import *
